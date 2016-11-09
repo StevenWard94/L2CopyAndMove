2141c6521d9048defa5553d720c4e5df9247c723
Steven Ward <stevenward94@gmail.com>
2016-11-08 20:32:02 -0600

    Add minor update to 'include/Node.h'

--- include/Node.h
+++ include/Node.h
@@ -1,7 +1,7 @@
 /***********************************************************************************//**
  * Author:       Steven Ward <stevenward94@gmail.com>
  * File:         /home/steven/lib/cpp.d/L2Copy+Move/include/Node.h
- * Last Change:  2016 Nov 07
+ * Last Change:  2016 Nov 08
  ***********************************************************************************/
 
 #ifndef LEARN_COPY_MOVE_INCLUDE_SINGLE_LINK_NODE_H_
@@ -17,34 +17,52 @@ namespace single_link {
 
 template<class E>
 class Node {
-    typedef E value_t;
-
     template<typename T>
     using remove_reference = typename std::remove_reference<T>::type;
+    using value_t = E;
 
   public:
     inline remove_reference<value_t> data( ) const { return std::forward<value_t>(data_); }
 
-    inline remove_reference<value_t> data(value_t const& val) {
-        value_t old_data = std::move(this->data_);
-        this->data_(val);
-        return old_data;
-    }
-
-    inline remove_reference<value_t> data(value_t&& val) {
-        value_t old_data = std::move(this->data_);
-        this->data_(std::forward<value_t>(val));
-        return old_data;
-    }
-
+    // assumes that a copy-assignment operator is defined for the type 'value_t'
+    inline void setData(value_t const& val) { this->data_ = val; }
+    inline remove_reference<value_t> data(value_t&& val) { this->data_(std::forward<value_t>(val)); }
 
     inline Node<value_t> const* const next( ) const { return const_cast<Node<value_t> const*>(next_); }
+    inline Node<value_t>* setNext(Node<value_t> const* node) { this->next_ = new Node<value_t>(node); }
 
-    inline Node<value_t>* next(Node<value_t> const* node) {
-        Node<value_t>* old_next = new Node<value_t>(this->next_);
-        this->next_ = new Node<value_t>(node);
-        return old_next;
+    inline Node<value_t>& operator= (Node<value_t> const& rhs) {
+        if (this != &rhs) {
+            delete this->next_;
+            this->next_ = nullptr;
+
+            Node<value_t> tmp(rhs);
+            std::swap(this->data_, tmp.data_);
+            std::swap(*(this->next_), (*(tmp.next_));
+        }
+        return *this;
     }
+
+    inline Node<value_t>& operator= (Node<value_t>&& rhs) {
+        delete this->next_;
+        this->next_ = nullptr;
+        std::swap(this->data_, rhs.data_);
+        std::swap(*(this->next_), *(rhs.next_));
+    }
+
+
+    inline Node( ) = default;
+    inline Node(value_t const& data) : data_(new value_t(data)) { }
+    inline Node(value_t&& data) : data_(std::move(data)) { }
+    inline Node(value_t const& data, Node<value_t> const* node)
+            : data_(new value_t(data)), next_(new Node<value_t>(node)) { }
+    inline Node(value_t&& data, Node<value_t> const* node)
+            : data_(std::move(data)), next_(new Node<value_t>(node)) { }
+
+    inline Node(Node<value_t> const& other)
+            : data_(new value_t(other.data_)), next_(new Node(other.next_)) { }
+
+    virtual ~Node( ) = default;
 
 
   private:
@@ -52,9 +70,16 @@ class Node {
     Node<value_t>* next_ { nullptr };
 
     inline Node(Node<value_t> const* node)
-            : data_ { node->data_ }, next_ { new Node<value_t>(node->next_) } { }
+            : data_(node->data_), next_(new Node<value_t>(node->next)) { }
+
+
+  friend void swap(Node<value_t>& lhs, Node<value_t>& rhs) {
+      using std::swap;
+      swap(lhs.data_, rhs.data_);
+      swap(*(lhs.next_), *(lhs.next_));
+  }
 };
 
-}
+}  // single_link
 
 #endif  // (LEARN_COPY_MOVE_INCLUDE_SINGLE_LINK_NODE_H_)
